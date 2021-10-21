97fd62cb85e22ab283e3f846b68e4ed895dab0be
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-10-22 03:18:27 +0600

    added array

---
+++ array.js
@@ -0,0 +1,25 @@
+var friendsAge = [42, 23, 21, 23];
+
+var muriChanachur = [45, 34, 100, 4];
+
+var friendsName = ['rakib', 'sakib', 'riyam'];
+
+var oddNumber = [1, 3, 5, 7];
+
+var nayikas = ['sabana', 'mahi', 'opu'];
+
+var vowles = ['A', 'E', 'I', 'O', 'U'];
+
+// console.log(friendsAge);
+// console.log(oddNumber);
+// console.log(nayikas);
+// console.log(vowles)
+
+
+// array element count
+var foodName = ['mango', 'banana', 'lichu'];
+var foodNameCount = foodName.length;
+console.log(foodNameCount);
+
+console.log(vowles.length);
+
