1b78852ce1c581963adcd6b1f9e9c467ea4379f7
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-10-22 01:55:48 +0600

    added js fundamentals File

---
+++ variable.js
@@ -0,0 +1,11 @@
+// string 
+var mouseName = 'logitech';
+console.log(typeof mouseName);
+
+// Number
+var mousePrice = 568;
+console.log(typeof mousePrice);
+
+// bollean 
+var isHot = false;
+console.log(typeof isHot);
