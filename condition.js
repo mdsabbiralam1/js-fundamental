2a465f0d9271413dd4a6a1d58bdfc3d5ba474a55
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-10-26 10:42:14 +0600

    add condition js

---
+++ condition.js
@@ -0,0 +1,23 @@
+var foodReady = true;
+// jodi khabar ready hoy taile ami khamo
+// if food is ready i will eat 
+// if food is ready == true;
+
+// if (condition){
+//                structer
+// }
+if (foodReady) {
+               console.log('age khabar khabo..tarpor porasona korbo');
+}
+
+var iphonePrice = 98000;
+var myBudget = 15000;
+if (iphonePrice < myBudget) {
+               console.log('iphone diye pic tule futani krbo');
+}
+
+if (iphonePrice > myBudget) {
+               console.log("xioami is the best phone");
+}
+
+
