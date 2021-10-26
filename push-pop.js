a728d39ca9ee6670be5748df6dcf67df67f8c029
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-10-26 08:13:35 +0600

    number add pop and push

--- push-pop.js
+++ push-pop.js
@@ -2,9 +2,16 @@
 var lastBencher = ['kalam', 'balam', 'jamal'];
 lastBencher.push('salam');
 lastBencher.push('borkot');
+
+var roolNumber = [34, 54, 534, 555, 34];
+roolNumber.push(55);
+console.log(roolNumber);
 // console.log(lastBencher);
 
 // variable pop
 var firstBencher = ['rakib', 'sakib', 'mamun', 'liton'];
 firstBencher.pop();
-console.log(firstBencher);
+var mousePrice = [23, 423, 423, 45, 42, 54];
+mousePrice.pop();
+console.log(mousePrice);
+// console.log(firstBencher);
