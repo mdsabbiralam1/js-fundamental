e393dd3c4485ea57667e3b0d4b2ff60838b825c7
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-10-26 08:30:28 +0600

    remove first element

--- push-pop.js
+++ push-pop.js
@@ -1,17 +1,22 @@
-// variable push
+// Add Element to array
 var lastBencher = ['kalam', 'balam', 'jamal'];
 lastBencher.push('salam');
 lastBencher.push('borkot');
 
 var roolNumber = [34, 54, 534, 555, 34];
 roolNumber.push(55);
-console.log(roolNumber);
+// console.log(roolNumber);
 // console.log(lastBencher);
 
-// variable pop
+// Remove an element from an array
 var firstBencher = ['rakib', 'sakib', 'mamun', 'liton'];
 firstBencher.pop();
 var mousePrice = [23, 423, 423, 45, 42, 54];
 mousePrice.pop();
-console.log(mousePrice);
+// console.log(mousePrice);
 // console.log(firstBencher);
+
+// remove first element from arry 
+var friendAges = [23, 43, 20, 53, 56];
+friendAges.shift();
+console.log(friendAges);
