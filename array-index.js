fdd88e4a0af3344b7816e7f8dcd52b378b68b24a
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-10-26 08:02:30 +0600

    variable push

---
+++ array-index.js
@@ -0,0 +1,22 @@
+var books = ['hablu', 'bablu', 'bolod', 'battery', 'honuman'];
+
+var batteryIndex = books.indexOf('battery');
+// console.log(batteryIndex);
+var secondIndex = books[2];
+// console.log(secondIndex);
+
+// next 
+var friendsName = ['mamun', 'rakib', 'sakib', 'tanvir', 'tusher'];
+var tusherIndex = friendsName.indexOf('tusher');
+// console.log(tusherIndex);
+
+// next
+var mathMarks = [34, 56, 75, 85];
+var matIndex = mathMarks.indexOf(75);
+
+mathMarks[3] = 45;
+console.log(mathMarks);
+// next
+var numbers = [34, 53, 534, 534, 3, 5];
+var index = numbers.indexOf(14);
+// console.log(index);
---
+++ push-pop.js
@@ -0,0 +1,6 @@
+// variable push
+var lastBencher = ['kalam', 'balam', 'jamal'];
+lastBencher.push('salam');
+lastBencher.push('borkot');
+console.log(lastBencher);
+
