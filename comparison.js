c541d3e44c7bbe69fd75caea150ef8e7910b956e
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-10-26 09:48:10 +0600

    adde love sectionr

---
+++ comparison.js
@@ -0,0 +1,12 @@
+// console.log(5 > 9);
+// console.log(5 < 6);
+// console.log(5 == 6);
+// console.log(6 == 6);
+// console.log(6 != 6 );
+
+var myLove = 60;
+var yourLove = 67;
+// console.log(myLove > yourLove);
+// console.log(myLove < yourLove);
+// console.log(myLove == yourLove);
+console.log(myLove != yourLove);
