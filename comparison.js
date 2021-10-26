c6ddb752a46e3b9ab87b927823c935429ee4defd
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-10-26 09:54:14 +0600

    added double compare

--- comparison.js
+++ comparison.js
@@ -4,9 +4,14 @@
 // console.log(6 == 6);
 // console.log(6 != 6 );
 
+// love section
 var myLove = 60;
 var yourLove = 67;
 // console.log(myLove > yourLove);
 // console.log(myLove < yourLove);
 // console.log(myLove == yourLove);
 console.log(myLove != yourLove);
+
+// double compare
+console.log(goodStudent && banker);
+console.log(goodStudent || banker);
