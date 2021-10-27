df2cfe349fede3bf1a0ee02ef3e03026a217850a
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-10-28 00:36:38 +0600

    added two condition

---
+++ multiple-condition.js
@@ -0,0 +1,9 @@
+var gotJob = true;
+var saveMoney = 150000;
+
+if (gotJob == true && saveMoney > 200000) {
+               console.log('colo biye kore feli');
+}
+else{
+               console.log('tor kopale biye nei');
+}
