0b7998700289f03e8a9d6a8728fb6e6d08d60f85
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-10-28 03:11:10 +0600

    added index part

---
+++ summary.js
@@ -0,0 +1,20 @@
+var tableLength = 45;
+var tourDastination = ['coxs-bazer', 'nepal', 'Butan'];
+
+var batteryIndex = tourDastination.indexOf('Butan');
+// console.log(batteryIndex);
+
+var thiredDastination = tourDastination[2]
+// console.log(thiredDastination);
+
+var butanIndex = tourDastination.indexOf('Butan');
+console.log(butanIndex);
+
+tourDastination[1] = 'Singapore';
+// console.log(tourDastination);
+
+tourDastination.push('London');
+// console.log(tourDastination);
+
+tourDastination.pop();
+console.log(tourDastination);
