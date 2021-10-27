e3be5ee37b419e22f503e59f9d13357600567596
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-10-28 00:46:44 +0600

    complex condition add

--- multiple-condition.js
+++ multiple-condition.js
@@ -1,7 +1,17 @@
 var gotJob = true;
 var saveMoney = 150000;
+var hasFlat = true;
 
-if (gotJob == true && saveMoney > 200000) {
+// two condition .....
+// if (gotJob == true && saveMoney > 200000) {
+//                console.log('colo biye kore feli');
+// }
+// else{
+//                console.log('tor kopale biye nei');
+// }
+
+// Complex condition
+if ((gotJob == true && saveMoney > 200000) || hasFlat == true)  {
                console.log('colo biye kore feli');
 }
 else{
