c01589f2dd1c4a23ac1b95dd710d17055c7093b5
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-10-28 01:17:58 +0600

    added nasted condition

---
+++ nasted.js
@@ -0,0 +1,18 @@
+var danisPrice = 50;
+var butterBreadPrice = 25;
+var toastBiscutPrice = 10;
+var myBudget = 25;
+
+//nasted  condition
+if (danisPrice < myBudget) {
+               console.log('danish kheye danish desh e jabo');
+}
+else if (butterBreadPrice <= myBudget) {
+               console.log('butter bread khabo cha diye');
+}
+else if (toastBiscutPrice < myBudget) {
+               console.log('toast biskut diye cha khyaoyar mojai alada');
+}
+else{
+               console.log('batasha diye cha khamo..!!!')
+}
